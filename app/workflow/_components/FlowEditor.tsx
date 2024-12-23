a2acddc75367b986b2c8340df6ccf5f71c4a8ab2
Anhadpreet Singh <94036563+Anhad928@users.noreply.github.com>
2024-12-23 13:01:52 -0700

    feat: added controls for flow-react

--- app/workflow/_components/FlowEditor.tsx
+++ app/workflow/_components/FlowEditor.tsx
@@ -1,16 +1,20 @@
 "use client";
 
 import { Workflow } from '@prisma/client';
-import { ReactFlow, useEdgesState, useNodesState } from '@xyflow/react';
+import { Controls, ReactFlow, useEdgesState, useNodesState } from '@xyflow/react';
 import React from 'react'
+
+import "@xyflow/react/dist/style.css";
 
 function FlowEditor({workflow}: {workflow: Workflow}) {
     const[nodes, setNodes, onNodesChange] = useNodesState([]);
     const [edges, setEdges, onEdgesChange] = useEdgesState([]);
   return (
     <main className='h-full w-full'>
-        <ReactFlow>
-
+        <ReactFlow nodes={nodes} edges={edges}
+        onEdgesChange={onEdgesChange}
+        onNodesChange={onNodesChange}>
+        <Controls position='top-left'/>
         </ReactFlow>
     </main>
   )
