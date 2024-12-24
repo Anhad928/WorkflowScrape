47070e5bd8cc6385234d7e225bce98ae8729f827
Anhadpreet Singh <94036563+Anhad928@users.noreply.github.com>
2024-12-24 00:06:43 -0700

    feat: added TaskType for LaunchBrowser

--- app/workflow/_components/FlowEditor.tsx
+++ app/workflow/_components/FlowEditor.tsx
@@ -7,6 +7,12 @@ import React from 'react'
 import "@xyflow/react/dist/style.css";
 import { CreateFlowNode } from '@/lib/workflow/createFlowNode';
 import { TaskType } from '@/types/task';
+import NodeComponent from './nodes/NodeComponent';
+
+const nodeTypes = {
+    Node: NodeComponent,
+
+}
 
 function FlowEditor({workflow}: {workflow: Workflow}) {
     const[nodes, setNodes, onNodesChange] = useNodesState([
@@ -19,7 +25,8 @@ function FlowEditor({workflow}: {workflow: Workflow}) {
     <main className='h-full w-full'>
         <ReactFlow nodes={nodes} edges={edges}
         onEdgesChange={onEdgesChange}
-        onNodesChange={onNodesChange}>
+        onNodesChange={onNodesChange} 
+        nodeTypes={nodeTypes}>
         <Controls position='top-left'/>
         <Background variant={BackgroundVariant.Dots} gap ={12} size={1} />
         </ReactFlow>
---
+++ app/workflow/_components/nodes/NodeCard.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React, { ReactNode } from "react";
+
+function NodeCard({children, nodeId}: { nodeId: string; children: ReactNode;}) {
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
+
+export default NodeCard;
