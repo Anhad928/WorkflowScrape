372abe17b9be475e6b1ae779de37003333b62012
Anhadpreet Singh <94036563+Anhad928@users.noreply.github.com>
2024-12-24 00:15:02 -0700

    feat: selected item highlight UI

--- app/workflow/_components/FlowEditor.tsx
+++ app/workflow/_components/FlowEditor.tsx
@@ -10,7 +10,7 @@ import { TaskType } from '@/types/task';
 import NodeComponent from './nodes/NodeComponent';
 
 const nodeTypes = {
-    Node: NodeComponent,
+    FlowScrapeNode: NodeComponent,
 
 }
 
--- app/workflow/_components/nodes/NodeCard.tsx
+++ app/workflow/_components/nodes/NodeCard.tsx
@@ -1,10 +1,13 @@
 "use client";
 
+import { cn } from "@/lib/utils";
 import React, { ReactNode } from "react";
 
-function NodeCard({children, nodeId}: { nodeId: string; children: ReactNode;}) {
+function NodeCard({children, nodeId, isSelected}: { nodeId: string; children: ReactNode; isSelected: boolean;}) {
   return (
-    <div>
+    <div className={cn("rounded-md cursor-pointer bg-background border-2 border-separate w-[420px] text-xs gap-1 flex flex-col",
+        isSelected && "border-primary"
+    )}>
       {children}
     </div>
   )
