361704444beacc6bec882434c6f3c160983fbf4c
Anhadpreet Singh <94036563+Anhad928@users.noreply.github.com>
2024-12-25 01:44:47 -0700

    feat: save button working

--- app/workflow/_components/topbar/SaveBtn.tsx
+++ app/workflow/_components/topbar/SaveBtn.tsx
@@ -1,16 +1,34 @@
 "use client";
 
+import { UpdateWorkflow } from '@/actions/workflows/updateWorkflow';
 import { Button } from '@/components/ui/button';
+import { useMutation } from '@tanstack/react-query';
 import { useReactFlow } from '@xyflow/react';
 import { CheckIcon } from 'lucide-react';
 import React from 'react'
+import { toast } from 'sonner';
 
 function SaveBtn({ workflowId }: { workflowId: string}) {
     const { toObject } = useReactFlow();
 
+    const saveMutation = useMutation({
+        mutationFn: UpdateWorkflow,
+        onSuccess:() => {
+            toast.success("Workflow saved successfully", {id: "save-workflow"});
+        },
+        onError: () => {
+            toast.error("Failed to save workflow", {id: "save-workflow"});
+        },
+    })
+
   return (
     <Button variant={"outline"} className='flex items-center gap-2' onClick={() => {
-        console.log("@FLOW", workflowId);
+        const workflowDefination = JSON.stringify(toObject());
+        toast.loading("Saving workflow", {id: "save-workflow"});
+        saveMutation.mutate({
+            id: workflowId,
+            defination: workflowDefination,
+        })
     }}
     >
         <CheckIcon size={16} className='stroke-green-400' />
