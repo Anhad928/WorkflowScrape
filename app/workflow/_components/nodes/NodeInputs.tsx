6f0f3c5210077bcb4e28b0b2bb41c19ee0e7e624
Anhadpreet Singh <94036563+Anhad928@users.noreply.github.com>
2024-12-24 21:49:17 -0700

    feat: inhanced UI of workflow

--- app/workflow/_components/nodes/NodeInputs.tsx
+++ app/workflow/_components/nodes/NodeInputs.tsx
@@ -1,3 +1,8 @@
+import { cn } from "@/lib/utils";
+import { TaskParam } from "@/types/task";
+import { Handle, Position } from "@xyflow/react";
+import NodeParamField from "./NodeParamField";
+
 export function NodeInputs({ children }: { children: React.ReactNode }) {
     return(
         <div className="flex flex-col dividy-y gap-2">
@@ -6,10 +11,14 @@ export function NodeInputs({ children }: { children: React.ReactNode }) {
     )
 }
 
-export function NodeInput({ input }: { input: any}) {
+export function NodeInput({ input }: { input: TaskParam}) {
     return (
         <div className="flex justify-start relative p-3 bg-secondary w-full">
-            <pre>{JSON.stringify(input, null, 4)}</pre>
+            <NodeParamField param={input}/>
+            {/* <pre>{JSON.stringify(input, null, 4)}</pre> */}
+            {!input.hideHandle && (
+            <Handle id={input.name} type="target" position={Position.Left}
+            className={cn("!bg-muted-foreground !border-2 !border-background !-left-2 !w-4 !h-4")}/>)}
         </div>
     );
 }
