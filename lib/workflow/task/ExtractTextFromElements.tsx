1c70f68451c42e96858fb9cf8cb08de43ebef5cc
Anhadpreet Singh <94036563+Anhad928@users.noreply.github.com>
2024-12-26 03:14:24 -0700

    feat: Add NodeParamField for BrowserInstanceParam and DeletableEdge component
    - Add NodeParamField for BrowserInstanceParam in TaskMenu component
    - Add TaskMenuBtn for TaskType.EXTRACT_TEXT_FROM_ELEMENT in TaskMenu component
    - Update DeletableEdge component to use getSimpleBezierPath and useReactFlow hook
    - Add delete button functionality to DeletableEdge component
    - Add ExtractTextFromElementTask to TaskRegistry
    - Add ExtractTextFromElementTask component with inputs and outputs

---
+++ lib/workflow/task/ExtractTextFromElements.tsx
@@ -0,0 +1,29 @@
+import { TaskParamType, TaskType } from "@/types/task";
+import { CodeIcon, Flashlight, GlobeIcon, LucideProps, TextIcon } from "lucide-react";
+
+export const ExtractTextFromElementTask = {
+    type: TaskType.EXTRACT_TEXT_FROM_ELEMENT,
+    label: "Get Text from HTML Element",
+    icon: (props: LucideProps) => (
+        <TextIcon className="stroke-rose-400" {...props} />
+    ),
+    isEntryPoint: false,
+    inputs: [
+        {
+            name: "HTML",
+            type: TaskParamType.STRING,
+            required: true,
+        },
+        {
+            name: "Selector",
+            type: TaskParamType.STRING,
+            required: true,
+        }
+    ],
+    outputs: [
+        {
+            name: "Extracted Text",
+            type: TaskParamType.STRING,
+        },
+    ],
+};
