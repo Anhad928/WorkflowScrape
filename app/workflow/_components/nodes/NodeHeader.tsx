656feea3a3048d586ea54c0404af2ef585e62432
Anhadpreet Singh <94036563+Anhad928@users.noreply.github.com>
2024-12-27 23:59:01 -0700

    feat: added copy and delete buttons on UI of node headers

--- app/workflow/_components/nodes/NodeHeader.tsx
+++ app/workflow/_components/nodes/NodeHeader.tsx
@@ -4,17 +4,19 @@ import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
 import { TaskRegistry } from '@/lib/workflow/task/registry';
 import { TaskType } from '@/types/task';
-import { CoinsIcon, GripVerticalIcon } from 'lucide-react';
+import { useReactFlow } from '@xyflow/react';
+import { CoinsIcon, CopyIcon, GripVerticalIcon, TrashIcon } from 'lucide-react';
 import React from 'react'
 
-function NodeHeader({ taskType }: { taskType: TaskType }) {
+function NodeHeader({ taskType, nodeId }: { taskType: TaskType, nodeId: string }) {
     const task = TaskRegistry[taskType]
+    const { deleteElements } = useReactFlow()
   return (
     <div className='flex items-center gap-2 p-2'>
       <task.icon size={16} />
       <div className='flex justify-between items-center w-full'>
         <p className='text-xs font-bold uppercase text-muted-foreground'>
-            {taskType}
+            {task.label}
         </p>
         <div className='flex gap-1 items-center'>
             {task.isEntryPoint && <Badge>Entry Point</Badge>}
@@ -22,6 +24,20 @@ function NodeHeader({ taskType }: { taskType: TaskType }) {
                 <CoinsIcon size={16}/>
                 TODO
             </Badge>
+            {!task.isEntryPoint && (
+              <>
+              <Button variant={"ghost"} size={"icon"} onClick={() => {
+                deleteElements({
+                  nodes:[{id: nodeId}]
+                })
+              }}>
+                <TrashIcon size={12}/>
+              </Button>
+              <Button variant={"ghost"} size={"icon"}>
+                <CopyIcon size={12}/>
+              </Button>
+              </>
+            )}
             <Button variant={"ghost"} size={"icon"} className='drag-handle cursor-grab'>
                 <GripVerticalIcon size={20}/>
             </Button>
