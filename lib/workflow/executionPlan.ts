fca10125bd5c4c68245328b6e69bad0815b8fe34
Anhadpreet Singh <94036563+Anhad928@users.noreply.github.com>
2024-12-28 01:38:48 -0700

    feat: add error handling for invalid inputs in execution plan generation

--- lib/workflow/executionPlan.ts
+++ lib/workflow/executionPlan.ts
@@ -43,10 +43,28 @@ export function FlowToExecutionPlan(
                     // If all incoming incomers/edges are planned and these are still invalid inputs,
                     // this means that this particular node has an invalid input.
                     // which means that the workflow is invalid.
+                    console.error("invalid inputs", currentNode.id, invalidInputs);
+                    throw new Error("TODO: HANDLE THIS ERROR 1");
+                } else {
+                    // let's skip this node for now
+                    continue;
                 }
             }
+            nextPhase.nodes.push(currentNode);
+            planned.add(currentNode.id);
         }
     }
 
     return {executionPlan};
+}
+
+function getInvalidInputs(node: AppNode, edges:Edge[], planned: Set<string>){
+    const invalidInputs =[];
+    const inputs = TaskRegistry[node.data.type].inputs;
+    for ( const input of inputs){
+        const inputValue = node.data.inputs[input.name];
+        const inputValueProvided = inputValue?.length > 0;
+    }
+        
+    
 }
