7abda271c9c0071033cd273e4f0736539580f5e0
Anhadpreet Singh <94036563+Anhad928@users.noreply.github.com>
2024-12-30 01:28:42 -0700

    Refactor ExecutionViewer component to include a reusable ExecutionLabel component and update dependencies

--- app/workflow/runs/[workflowId]/[executionId]/_components/ExecutionViewer.tsx
+++ app/workflow/runs/[workflowId]/[executionId]/_components/ExecutionViewer.tsx
@@ -1,10 +1,14 @@
 "use client";
 
 import { GetWorkflowExecutionWithPhases } from '@/actions/workflows/getWorkflowExecutionWithPhases';
+import { Badge } from '@/components/ui/badge';
+import { Button } from '@/components/ui/button';
+import { Separator } from '@/components/ui/separator';
 import { WorkflowExecutionStatus } from '@/types/workflow';
+
 import { useQuery } from '@tanstack/react-query';
 import { formatDistanceToNow } from 'date-fns';
-import { CalendarIcon, CircleDashedIcon, LucideIcon } from 'lucide-react';
+import { CalendarIcon, CircleDashedIcon, ClockIcon, CoinsIcon, LucideIcon, WorkflowIcon } from 'lucide-react';
 import React, { ReactNode } from 'react'
 
 type ExecutionData = Awaited<ReturnType<typeof GetWorkflowExecutionWithPhases>>
@@ -20,6 +24,8 @@ export default function ExecutionViewer({
         queryFn: () => GetWorkflowExecutionWithPhases(initialData!.id),
         refetchInterval: (q) => q.state.data?.status === WorkflowExecutionStatus.RUNNING ? 1000 : false,
     });
+
+    const duration = DatesToDurationString(query.data?.completedAt, query.data?.startedAt);
   return (
     <div className='flex w-full h-full'>
       <aside className='w=[440px] min-w-[440px] max-w-[440px] border-r-2
@@ -27,11 +33,45 @@ export default function ExecutionViewer({
         <div className='py-4 px-2'>
             {/* Status label */}
             <ExecutionLabel icon={CircleDashedIcon} label="Status" value={query.data?.status} />
-            <ExecutionLabel icon={CalendarIcon} label="Started at" value={query.data?.startedAt ? formatDistanceToNow(new Date(query.data?.startedAt), {
+            <ExecutionLabel icon={CalendarIcon} label="Started at" 
+            value={
+                <span className='lowercase'>
+                {query.data?.startedAt ? formatDistanceToNow(new Date(query.data?.startedAt), {
                         addSuffix: true,
 
-                    }) : "-"} />
+                    }) : "-"}
+                    </span>
+                     }
+                     />
+            <ExecutionLabel 
+                icon = {ClockIcon}
+                label = "Duration"
+                value = {"TODO"}
+            />
+            <ExecutionLabel 
+                icon = {CoinsIcon}
+                label = "Credits consumed"
+                value = {"TODO"}
+            />
 
+        </div>
+        <Separator />
+        <div className='flex justify-center items-center py-2 px-4'>
+            <div className='text-muted-foreground flex items-center gap-2'>
+                <WorkflowIcon size={20} className='stroke-muted-foreground/80' />
+                <span className='font-semibold'>Phases</span>
+            </div>
+        </div>
+        <Separator />
+        <div className='overflow-auto h-full px-2 py-4'>
+            {query.data?.phases.map((phase, index) => (
+                <Button key={phase.id} className='w-full justify-between' variant={"ghost"}>
+                    <div className='flex items-center gap-2'>
+                        <Badge variant={"outline"}>{index + 1}</Badge>
+                        <p className='font-semibold'>{phase.name}</p>
+                    </div>
+                </Button>
+            ))}
         </div>
       </aside>
     </div>
