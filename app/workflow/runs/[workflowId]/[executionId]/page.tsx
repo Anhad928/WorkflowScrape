436139354067218bccdb2f43d0a2f864384e2b2e
Anhadpreet Singh <94036563+Anhad928@users.noreply.github.com>
2024-12-29 23:19:54 -0700

    Refactor ExecutionViewersPage component to include a suspense fallback and a wrapper component for execution viewers

--- app/workflow/runs/[workflowId]/[executionId]/page.tsx
+++ app/workflow/runs/[workflowId]/[executionId]/page.tsx
@@ -1,4 +1,7 @@
 import Topbar from "@/app/workflow/_components/topbar/Topbar";
+import { waitFor } from "@/lib/helper/waitFor";
+import { Loader2Icon } from "lucide-react";
+import { Suspense } from "react";
 
 export default function ExecutionViewersPage({
     params,
@@ -15,6 +18,29 @@ export default function ExecutionViewersPage({
             subtitle = {`Run ID: ${params.executionId}`}
             hideButtons
             />
+            <section className="flex h-full overflow-auto">
+                <Suspense fallback={
+                    <div className="flex w-full items-center justify-center">
+                        <Loader2Icon className="h-10 w-10 animate-spin stroke-primary"/>
+                    </div>
+                }>
+                    <ExecutionViewersWrapper executionId={params.executionId} />
+                </Suspense>
+            </section>
+        </div>
+    );
+}
+
+
+async function ExecutionViewersWrapper({
+    executionId,
+}: {
+    executionId: string;
+}) {
+    await waitFor(5000 );
+    return (
+        <div>
+            Wrapper
         </div>
     )
 }
