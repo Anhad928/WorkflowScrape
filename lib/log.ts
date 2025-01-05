feb26189ebfb9647ae8db62c99efee6328777562
Anhadpreet Singh <94036563+Anhad928@users.noreply.github.com>
2025-01-05 02:31:13 -0700

    Refactor log.ts to include log functions for different log levels

--- lib/log.ts
+++ lib/log.ts
@@ -1,8 +1,13 @@
-import { Log, LogCollector } from "@/types/log";
+import { Log, LogCollector, LogFunction, LogLevel, LogLevels } from "@/types/log";
 
 export function createLogCollector(): LogCollector {
     const logs: Log[] = [];
     const getAll = () => logs;
+
+    const logFunctions = {} as Record<LogLevel, LogFunction>;
+    LogLevels.forEach((level) => logFunctions[level] = (message:string) => {
+        logs.push({ message, level, timestamp: new Date() });
+    })
     return {
         getAll,
         info: (message:string) => logs.push({ level: "info", "message": message,
