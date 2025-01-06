c66cf8c574735cfb56c120e620aca1440ee0ff38
Anhadpreet Singh <94036563+Anhad928@users.noreply.github.com>
2025-01-06 01:53:04 -0700

    Add GetAvailableCredits function to retrieve user's available credits

---
+++ components/UserAvailableCreditsBadge.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from 'react'
+
+function UserAvailableCreditsBadge() {
+  return (
+    <div>
+      UserAvaiableCreditsBadge
+    </div>
+  )
+}
+
+export default UserAvailableCreditsBadge
