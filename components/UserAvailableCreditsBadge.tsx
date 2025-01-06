f4a1c7e303bc4864ddb36d40fef5e835ce8e8de2
Anhadpreet Singh <94036563+Anhad928@users.noreply.github.com>
2025-01-06 02:05:09 -0700

    Refactor UserAvailableCreditsBadge component and add utility function
    - Refactor the UserAvailableCreditsBadge component to import the 'cn' utility function from '@/lib/utils'.
    - Add a utility function to retrieve the user's available credits using the GetAvailableCredits action.
    - Update the link in the UserAvailableCreditsBadge component to include the 'w-full space-x-2 items-center' class.
    - Add the 'react-countup' package as a dependency in the package.json file.

--- components/UserAvailableCreditsBadge.tsx
+++ components/UserAvailableCreditsBadge.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { GetAvailableCredits } from '@/actions/billing/getAvailableCredits';
+import { cn } from '@/lib/utils';
 import { useQuery } from '@tanstack/react-query';
 import { CoinsIcon, Loader2Icon } from 'lucide-react';
 import Link from 'next/link';
@@ -9,11 +10,11 @@ import React from 'react'
 function UserAvailableCreditsBadge() {
     const query = useQuery({
         queryKey: ["user-available-credits"],
-        queryFn: GetAvailableCredits,
+        queryFn: () => GetAvailableCredits(),
         refetchInterval: 30 * 1000, // 30 seconds
     })
   return (
-    <Link href={"/billing"}>
+    <Link href={"/billing"} className={cn("w-full space-x-2 items-center")}>
         <CoinsIcon size={20} className='text-primary'/>
         <span className='font-semibold capitalize'>
             {query.isLoading && <Loader2Icon className='w-4 h-4 animate-spin'/>}
