72659d8b91e080ecf4f59f2e64b5f9226c01e4a3
Anhadpreet Singh <94036563+Anhad928@users.noreply.github.com>
2025-01-08 13:57:47 -0700

    Update SchedulerDialog component to include a form for specifying a cron expression and saving the schedule

--- app/(dashboard)/workflows/_components/SchedulerDialog.tsx
+++ app/(dashboard)/workflows/_components/SchedulerDialog.tsx
@@ -10,12 +10,26 @@ import {
     DialogHeader,
     DialogTrigger,
 } from "@/components/ui/dialog";
-import { TriangleAlertIcon } from 'lucide-react';
+import { CalendarIcon, TriangleAlertIcon } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
 import CustomDialogHeader from '@/components/CustomDialogHeader';
+import { Input } from '@/components/ui/input';
+import { useMutation } from '@tanstack/react-query';
+import { toast } from 'sonner';
+import { UpdateWorkflowCron } from '@/actions/workflows/updateWorkflowCron';
 
 export default function SchedulerDialog() {
+
+    const mutation = useMutation({
+        mutationFn: UpdateWorkflowCron,
+        onSuccess: () => {
+            toast.success("Schedule updated successfully", { id: "cron"})
+        },
+        onError: () => {
+            toast.error("Something went wrong", { id: "cron" })
+        }
+    })
   return (
     <Dialog>
         <DialogTrigger asChild>
@@ -26,7 +40,26 @@ export default function SchedulerDialog() {
             </Button>
         </DialogTrigger>
         <DialogContent className='px-0'>
-            <CustomDialogHeader title='Schedule workflow execution'/>
+            <CustomDialogHeader title='Schedule workflow execution' 
+            icon ={CalendarIcon}
+            />
+            <div className="p-6 space-y-4">
+                <p className='text-muted-foreground text-sm'>
+                    Specify a cron expression to Schedule periodic workflow execution.
+                    All time are in UTC
+                </p>
+                <Input placeholder='E.g. * * * * *' />
+            </div>
+            <DialogFooter className='px-6 gap-2'>
+                <DialogClose asChild>
+                    <Button className='w-full' variant={"secondary"}>
+                        Cancel
+                    </Button>
+                </DialogClose>
+                <DialogClose asChild>
+                    <Button className='w-full'>Save</Button>
+                </DialogClose>
+            </DialogFooter>
         </DialogContent>
     </Dialog>
   )
