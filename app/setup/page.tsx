fe7c2da734cda38f419c84c47b7865dfebb287d2
Anhadpreet Singh <94036563+Anhad928@users.noreply.github.com>
2025-01-15 23:57:54 -0700

    setup page and sidebar component

--- app/setup/page.tsx
+++ app/setup/page.tsx
@@ -2,7 +2,6 @@ import { SetupUser } from "@/actions/billing/setupUser";
 import { waitFor } from "@/lib/helper/waitFor";
 
 export default async function SetupPage() {  
-    await waitFor(5000);
     return await SetupUser();
     
 }
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -12,7 +12,7 @@ import UserAvailableCreditsBadge from './UserAvailableCreditsBadge';
 
 const routes = [
     {
-        href: "/",
+        href: "",
         label: "Home",
         icon: HomeIcon,
     },
@@ -48,7 +48,7 @@ function DesktopSidebar() {
         <div className='p-2'><UserAvailableCreditsBadge/></div>
       <div className='flex flex-col p-2'>
         {routes.map(route => (
-            <Link key={route.href} href={route.label}
+            <Link key={route.href} href={`/${route.href}`}
                 className={buttonVariants({
                     variant: activeRoute.href === route.href ? 'sidebarActiveItem' : 'sidebarItem',
                 })}>
@@ -85,7 +85,7 @@ export function MobileSidebar() {
                     <UserAvailableCreditsBadge/>
                     <div className='flex flex-col gap-1'>
                     {routes.map(route => (
-                        <Link key={route.href} href={route.label}
+                        <Link key={route.href} href={`/${route.href}`}
                             className={buttonVariants({
                                 variant: activeRoute.href === route.href ? 'sidebarActiveItem' : 'sidebarItem',
                             })}
