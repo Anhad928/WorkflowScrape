e67a46246369f74b7da2dd2ed196c9013ad35ae7
Anhadpreet Singh <94036563+Anhad928@users.noreply.github.com>
2025-01-16 15:48:57 -0700

    Refactor webhook route to handle checkout session completed event and update user balance with purchased credits

--- lib/stripe/handleCheckoutSessionCompleted.ts
+++ lib/stripe/handleCheckoutSessionCompleted.ts
@@ -1,9 +1,38 @@
+import { getCreditsPack, PackId } from "@/types/billing";
 import { writeFile } from "fs";
 import "server-only";
 import Stripe from "stripe";
+import prisma from "../prisma";
 
 export async function HandleCheckoutSessionCompleted(
     event: Stripe.Checkout.Session
 ) {
-    writeFile("session_completed.json", JSON.stringify(event), (err) => {})
+    if(!event.metadata){
+        throw new Error("No metadata");
+    }
+    const {userId, packId} = event.metadata;
+    if (!userId) {
+        throw new Error("No user ID");
+    }
+    if (!packId) {
+        throw new Error("No pack ID");
+    }
+
+    const purchasePack = getCreditsPack(packId as PackId);
+    if (!purchasePack) {
+        throw new Error("Invalid pack");
+    }
+
+    await prisma.userBalance.upsert({
+        where: { userId },
+        create: {
+            userId,
+            credits: purchasePack.credits,
+        },
+        update: {
+            credits: {
+                increment: purchasePack.credits,
+            },
+        },
+    });
 }
